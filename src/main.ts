6c2e470513cc6f246f82dd3c70c7ef6af141d99d
Heliomar P. Marques <heliomarpm@gmail.com>
2023-02-28 23:14:28 -0300

    feat: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '@fortawesome/fontawesome-free/css/all.css'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
